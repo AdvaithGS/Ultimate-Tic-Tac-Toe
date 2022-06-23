bafa7c265938c71e025e282950167d2ddc003c88
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-06-23 16:29:36 +0530

    added board class

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from itertools import cycle
 from termcolor import colored
 class Player():
-  def __init__(self,name: str,sign: str,wins: list):
+  def __init__(self,name: str,sign: str):
     self.name = name
     self.sign = sign
-    self.wins = wins
   def __str__(self):
     return self.name
 
-p1 = Player(input('Enter player 1 (X) name: '),'X',[])
-p2 = Player(input('Enter player 2 (O) name: '),'O',[])
+class Board():
+  def __init__(self,board:list,won:list):
+    self.board = board
+    self.won = won
+  def __str__(self):
+    return self.board
+  
+p1 = Player(input('Enter player 1 (X) name: '),'X')
+p2 = Player(input('Enter player 2 (O) name: '),'O')
 
-board = [['-']*9 for i in range(9)]
+board = Board([['-']*9 for i in range(9)],['-' for i in range(9)]
 def show():
   for b in range(3):
     for a in range(3):
