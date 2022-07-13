e84e94c7541dbbb23e13ea171b775da1fb12b6c8
AdvaithGS <advaith.gs4@gmail.com>
2022-07-13 18:54:34 +0530

    adding main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from stat import S_ENFMT
-
-
 s = input('Choose 1 for 2-player and 2 for multiplayer: ')
 while not s.isdigit() or int(s) not in range(1,3):
   print('Input must be either 1 or 2')
@@ -9,4 +6,4 @@ s = int(s)
 if s == 1:
   import two_player
 elif s == 2:
-  import multi_player
+  import multiplayer.menu
