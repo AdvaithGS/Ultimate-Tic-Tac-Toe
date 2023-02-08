a7b2d92d693dcd373d575e018ecfb1511cb0d3a3
AdvaithGS <advaith.gs4@gmail.com>
2023-02-08 19:13:23 +0530

    update menu.py

--- multiplayer/menu.py
+++ multiplayer/menu.py
@@ -1,8 +1,9 @@
-s = input('Do you want to host or join an existing match? 1:host, 2:join- ')
+import os
+s = input('Do you want to host or join an existing match? 1:host, 2:join -')
 while not s.isdigit() or int(s) not in [1,2]:
-  s = input('Do you want to host or join an existing match? 1:host, 2:join- ')
+  s = input('Do you want to host or join an existing match? 1:host, 2:join -')
 s = int(s)
 if s == 1:
   import multiplayer.multiplayer
-else:
+elif s == 2:
   import multiplayer.client
